3ea35c35cce023b41b9b7b50f874c73036712d72
ram <ramkrishna.jana@42hertz.com>
2018-08-27 16:13:23 +0530

    Base

---
+++ FdxProductPlugin/CRMQueryExpression.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FdxProductPlugin
+{
+    public class CRMQueryExpression
+    {
+        public static EntityCollection getOpportunityProducts(Guid _productId, IOrganizationService _service)
+        {
+            FetchExpression query;
+            EntityCollection oppProducts = new EntityCollection();
+            string OppProductQuery = "<fetch top='5000' ><entity name='product' ><attribute name='fdx_revenuetype' /><attribute name='productid' /><attribute name='name' /><filter type='and' ><condition attribute='productid' operator='eq' value='{0}' /></filter><link-entity name='opportunityproduct' from='productid' to='productid' link-type='inner' ><attribute name='fdx_nrr' /><attribute name='fdx_mrr' /><attribute name='opportunityproductid' alias='opportunityId'/><attribute name='fdx_revenuetype' /><link-entity name='opportunity' from='opportunityid' to='opportunityid' link-type='inner' ><attribute name='statecode' /><attribute name='name' /><filter type='and' ><condition attribute='statecode' operator='eq' value='0' /></filter></link-entity></link-entity></entity></fetch>";
+
+            query = new FetchExpression(string.Format(OppProductQuery, _productId));
+            oppProducts = _service.RetrieveMultiple(query);
+
+            return oppProducts;
+        }
+    }
+}
---
+++ FdxProductPlugin/Product_Update.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FdxProductPlugin
+{
+    public class Product_Update : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            //Extract the tracing service for use in debugging sandboxed plug-ins....
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            //Obtain execution contest from the service provider....
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            int step = 0;
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity productEntity = (Entity)context.InputParameters["Target"];
+
+                if (productEntity.LogicalName != "product")
+                    return;
+
+                Entity oppProduct = new Entity();
+                EntityCollection oppProducts = new EntityCollection();
+                Guid oppProductId = Guid.Empty;
+
+                try
+                {
+                    step = 1;
+                    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                    //Get current user information....
+                    step = 2;
+                    WhoAmIResponse response = (WhoAmIResponse)service.Execute(new WhoAmIRequest());
+
+                    //Retrieve Opportunity products from Product....
+                    step = 3;
+                    if (productEntity.Attributes.Contains("fdx_revenuetype"))
+                    {
+                        oppProducts = CRMQueryExpression.getOpportunityProducts(productEntity.Id, service);
+                        step = 4;
+                        foreach(Entity oppProd in oppProducts.Entities)
+                        {
+                            step = 5;
+                            oppProductId = new Guid(((AliasedValue)oppProd.Attributes["opportunityId"]).Value.ToString());
+
+                            step = 6;
+                            oppProduct = new Entity
+                            {
+                                Id = oppProductId,
+                                LogicalName = "opportunityproduct"
+                            };
+
+                            step = 7;
+                            oppProduct.Attributes["fdx_revenuetype"] = productEntity.Attributes["fdx_revenuetype"];
+                            step = 8;
+                            service.Update(oppProduct);
+                        }
+                    }
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException(string.Format("An error occurred in the Product_Update plug-in at Step {0}.", step), ex);
+                }
+                catch (Exception ex)
+                {
+                    tracingService.Trace("Product_Update: step {0}, {1}", step, ex.ToString());
+                    throw;
+                }
+
+            }
+        }
+    }
+}
